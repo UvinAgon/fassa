013f8e1e31aaade1a53247df1a0caae88e233043
uvin <uvin@echonlabs.com>
2022-12-05 06:19:55 +0530

    onchange transaction data

--- src/popups/addTransaction.js
+++ src/popups/addTransaction.js
@@ -6,23 +6,23 @@ function AddTransaction(){
     const [amount, setAmount] = useState(0);
     const [description, setDescription] = useState('');
     // alert(date)
-    // save(){
-    //     setAmount= data.target.value
-    // };
+    function save(){
+        console.log(`${title} ${date} ${amount} ${description}`)
+    };
     return(
-        <div style={{display: `flex`, flexDirection: `row`,width: `500px`, height: `340px`}}>
-                <div style={{backgroundImage: `linear-gradient(135deg,#2b66a0 15%,#041023 65%)`, padding:'10px'}} className='col-md-11'>
+        <div style={{display: `flex`, flexDirection: `row`, width: `500px`, height: `340px`, borderEndEndRadius:`10px`}}>
+                <div style={{backgroundImage: `linear-gradient(135deg,#c0c7cc 15%,#525e66 65%)`, padding:'10px'}} className='col-md-11'>
                     <div style={{height:'50px',padding:7,color:'#b9cbe0f8'}}>
                         <label><b>Add Expense</b></label>
                     </div>
 
                     <form action="">
                         <div className='input-group mb-3'>
-                            <div style={{width:'110px'}} className='input-group-prepend'>
+                            <div style={{width:'410px'}} className='input-group-prepend'>
                                 <span className='input-group-text' id='inputGroup-sizing-default'>Date</span>
                             </div>
                             <input id='trans-date' type="date" className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm'
-                            />
+                                value={date} onChange={(e) => setDate(e.target.value)}/>
                         </div>
 
                         <div className='input-group mb-3'>
@@ -38,11 +38,11 @@ function AddTransaction(){
                                 <span className='input-group-text' id='inputGroup-sizing-default'>Description</span>
                             </div>
                             <input id='trans-description' type="text" className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm'
-                            value={description} />
+                                value={description} onChange={(e) => setDescription(e.target.value)}/>
                         </div>
 
-                        <div style={{display: `flex`,flexDirection: `row-reverse`,paddingTop:20}}>
-                            <button style={{ width: '80px',marginLeft:5}} className="btn btn-success btn-sm" ><b>Save</b></button>
+                        <div style={{display: `flex`,flexDirection: `row-reverse`,paddingTop:30}}>
+                            <button style={{ width: '80px',marginLeft:5}} className="btn btn-success btn-sm" onClick={save()}><b>Save</b></button>
                             <button style={{ width: '80px',marginLeft:5}} className="btn btn-danger btn-sm" ><b>Cancel</b></button>
                             <button style={{ width: '80px',marginLeft:5}} className="btn btn-secondary btn-sm" ><b>Reset</b></button>
                         </div>
